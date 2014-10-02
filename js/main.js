69a8829861e1ae535a68f8564a5d159206498251
Jade Dominguez <plusjade@gmail.com>
2014-10-01 19:20:47 -0700

    Add click functions for all links.

--- js/main.js
+++ js/main.js
@@ -12,4 +12,39 @@ $(document).ready(function() {
         // all sections already have ids so I'll just use that.
         $('#international-drop').slideToggle(150);
     })
+
+    // politics
+    $('#primary-nav .politics').click(function(e) {
+        e.preventDefault();
+
+        $('#politics-drop').slideToggle(150);
+    })
+
+    // business
+    $('#primary-nav .business').click(function(e) {
+        e.preventDefault();
+
+        $('#business-drop').slideToggle(150);
+    })
+
+    // technology
+    $('#primary-nav .technology').click(function(e) {
+        e.preventDefault();
+
+        $('#technology-drop').slideToggle(150);
+    })
+
+    // culture
+    $('#primary-nav .culture').click(function(e) {
+        e.preventDefault();
+
+        $('#culture-drop').slideToggle(150);
+    })
+
+    // blogs
+    $('#primary-nav .blogs').click(function(e) {
+        e.preventDefault();
+
+        $('#blogs-drop').slideToggle(150);
+    })
 });
