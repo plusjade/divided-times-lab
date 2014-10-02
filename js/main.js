8db40d349cd684477026b948abd6cedd31a08855
Jade Dominguez <plusjade@gmail.com>
2014-10-01 20:08:01 -0700

    I need to know if the visible element is the element that I'm trying to toggle because it should use slide functionality again.

--- js/main.js
+++ js/main.js
@@ -9,15 +9,28 @@ $(document).ready(function() {
         // I need to know if any section is currently visible so I can 
         // quickly show another section rather than having the choppy affect of sliding it down.
         // I also need to save the value because I'm going to hide them all directly after this.
-        var visible = $('#slide-down section').is(':visible');
+        var visible = $('#slide-down section:visible');
 
         // I want to automatically hide all sections
         // EXCEPT the current section im on
         // because I need to preserve its open/close state in order to toggle it.
         $('#slide-down section').not('#' + name + '-drop').hide();
 
-        if( visible ) {
-            $('#' + name + '-drop').toggle();
+        // jQuery selectors (visible) always return an Array, but it may be empty so we need to check.
+        if( visible.length > 0 ) {
+
+            // All sections have classNames that identify themselves so let's check that.
+            // This answers the question:
+            //    "is the visible section the section that we are trying to toggle?"
+            if(visible.hasClass(name)) {
+                // if the visible section is the one being toggled
+                // then we need to use the slideDown to achieve a nicer effect.
+                $('#' + name + '-drop').slideToggle(150);
+            }
+            else {
+                // Toggle is the same as hide() and show() just toggled.
+                $('#' + name + '-drop').toggle();
+            }
         }
         else {
             // all sections already have ids so I'll just use that.
