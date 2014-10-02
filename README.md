3dc8aecc0effede4bd3889a59ba27fc1b533a84e
Jade Dominguez <plusjade@gmail.com>
2014-10-01 19:09:16 -0700

    one

---
+++ README.md
@@ -0,0 +1,4 @@
+
+# remove automatic hiding.
+
+I don't like that the parent is being hidden so I'm going to unhide it and then hide all the invidual sectiosn because that's what we really care about.
---
+++ js/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // I don't like that the parent is being hidden so I'm going to unhide it.
+    $('#slide-down').show();
+    // all sections are childen of #slide-down so to start we should hide them all.
+    $('#slide-down section').hide();
+});
